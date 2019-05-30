68a3633a91e725a5384c457c5df93e4034687f3d
talharahman <talharahman@pursuit.org>
2019-05-29 21:27:04 -0400

    fixed petition host activity

--- app/src/main/java/com/example/grassroots/activity/PetitionHostActivity.java
+++ app/src/main/java/com/example/grassroots/activity/PetitionHostActivity.java
@@ -1,13 +1,18 @@
 package com.example.grassroots.activity;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.example.grassroots.R;
 import com.example.grassroots.fragment.petition.PetitionFirstFragment;
 import com.example.grassroots.utils.PetitionFragmentsListener;
 
-public class PetitionHostActivity extends AppCompatActivity implements PetitionFragmentsListener {
+public class PetitionHostActivity extends AppCompatActivity implements PetitionFragmentsListener, BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,32 @@ public class PetitionHostActivity extends AppCompatActivity implements PetitionF
                 .replace(R.id.petition_container, new PetitionFirstFragment())
                 .addToBackStack(null)
                 .commit();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_view_main);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.bot_nav_action:
+                Intent petitionIntent = new Intent(this, PetitionHostActivity.class);
+                startActivity(petitionIntent);
+                return true;
+            case R.id.bot_nav_event:
+                // TODO provision for Events UI
+                return true;
+            case R.id.bot_nav_contact:
+                Intent contactIntent = new Intent(this, LocalRepsActivity.class);
+                contactIntent.putExtra("ZIP", "11101");
+                startActivity(contactIntent);
+                return true;
+            case R.id.bot_nav_search:
+                Intent searchIntent = new Intent(this, CongressActivity.class);
+                startActivity(searchIntent);
+                return true;
+        }
+        return true;
     }
 
     @Override
