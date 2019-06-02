ff2acc2d544a7b1dc90800d5f0bc755c73735d7c
garlicfiendck <carolinekang@pursuit.org>
2019-06-02 18:43:49 -0400

    add changes

--- app/src/main/java/com/example/grassroots/recyclerview/ExpenseAdapter.java
+++ app/src/main/java/com/example/grassroots/recyclerview/ExpenseAdapter.java
@@ -2,16 +2,13 @@ package com.example.grassroots.recyclerview;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.grassroots.R;
 import com.example.grassroots.model.ProPublica.OfficeExpenses.OfficeExpResult;
-import com.example.grassroots.model.ProPublica.OfficeExpenses.OfficeExpenseResponse;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseViewHolder> {
