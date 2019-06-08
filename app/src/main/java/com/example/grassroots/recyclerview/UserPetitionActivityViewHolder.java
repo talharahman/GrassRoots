857081a5042d561680791fbe5241d247a921c0b9
rzmorales <robertzaratemorales@pursuit.org>
2019-06-08 09:18:01 -0400

    add viewholder for user activities

--- app/src/main/java/com/example/grassroots/recyclerview/UserPetitionActivityViewHolder.java
+++ app/src/main/java/com/example/grassroots/recyclerview/UserPetitionActivityViewHolder.java
@@ -4,8 +4,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-public class UserPetitionActivity extends RecyclerView.ViewHolder {
-    public UserPetitionActivity(@NonNull View itemView) {
+public class UserPetitionActivityViewHolder extends RecyclerView.ViewHolder {
+    public UserPetitionActivityViewHolder(@NonNull View itemView) {
         super(itemView);
     }
 
